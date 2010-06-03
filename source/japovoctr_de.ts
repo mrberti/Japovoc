5c960e387afc3cc196032a855bb1f1d6b87a51db
Simon Bertling <berti.x@gmx.de>
2010-06-03 19:56:07 +0200

    added translations

---
+++ source/japovoctr_de.ts
@@ -0,0 +1,313 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="de_DE">
+<context>
+    <name>AboutDialog</name>
+    <message>
+        <location filename="aboutdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="29"/>
+        <source>Japovoc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="aboutdialog.ui" line="55"/>
+        <source>日本語</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="77"/>
+        <source>Version 1.0</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="84"/>
+        <source>Author: Simon Bertling</source>
+        <translation>Autor: Simon Bertling</translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="91"/>
+        <source>Email: berti.x@gmx.de</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="aboutdialog.ui" line="98"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;MS Shell Dlg 2&apos;; font-size:8.25pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:8pt;&quot;&gt;Web: &lt;/span&gt;&lt;a href=&quot;http://github.com/mrberti/Japovoc&quot;&gt;&lt;span style=&quot; text-decoration: underline; color:#0000ff;&quot;&gt;http://github.com/mrberti/Japovoc&lt;/span&gt;&lt;/a&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Japovoc</name>
+    <message>
+        <location filename="japovoc.ui" line="14"/>
+        <location filename="japovoc.ui" line="30"/>
+        <source>Japovoc</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="japovoc.ui" line="56"/>
+        <source>ジャポヴォク</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="japovoc.ui" line="81"/>
+        <source>あいおうえお</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="88"/>
+        <source>Magic Button</source>
+        <translation>Magischer Knopf</translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="107"/>
+        <source>%v/%m</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="126"/>
+        <source>File</source>
+        <translation>Datei</translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="133"/>
+        <source>Edit</source>
+        <translation>Bearbeiten</translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="139"/>
+        <source>?</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="158"/>
+        <source>Quit</source>
+        <translation>Beenden</translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="163"/>
+        <source>Preferences...</source>
+        <translation>Einstellungen...</translation>
+    </message>
+    <message>
+        <location filename="japovoc.ui" line="168"/>
+        <source>About...</source>
+        <translation>Über...</translation>
+    </message>
+    <message>
+        <location filename="japovoc.cpp" line="80"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>Preferences</name>
+    <message>
+        <location filename="preferences.ui" line="14"/>
+        <source>Japovoc Preferences</source>
+        <translation>Japovoc Einstellungen</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="30"/>
+        <source>&amp;General</source>
+        <translation>Allgemein</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="36"/>
+        <source>&amp;Fonts</source>
+        <translation>Schriftarten</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="48"/>
+        <source>The font the Kanji appears.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="51"/>
+        <source>Kanji font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="preferences.ui" line="80"/>
+        <source>日本語</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="87"/>
+        <location filename="preferences.ui" line="139"/>
+        <location filename="preferences.ui" line="189"/>
+        <location filename="preferences.ui" line="239"/>
+        <location filename="preferences.ui" line="291"/>
+        <location filename="preferences.ui" line="341"/>
+        <source>Edit...</source>
+        <translation>Bearbeiten...</translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="98"/>
+        <source>The font the primary reagin appears.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="101"/>
+        <source>Primary reading font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="preferences.ui" line="132"/>
+        <source>カタカナ</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="150"/>
+        <source>The font the readings appear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="153"/>
+        <source>Reading font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="preferences.ui" line="182"/>
+        <source>ひらがな</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="200"/>
+        <source>The font the okurigana appear.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="203"/>
+        <source>Okurigana font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message utf8="true">
+        <location filename="preferences.ui" line="232"/>
+        <source>おくりがな</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="250"/>
+        <source>The font the primary translation appears.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="253"/>
+        <source>Primary translation font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="284"/>
+        <source>Primary translation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="302"/>
+        <source>The font the Translation appears.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="305"/>
+        <source>Translation font:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="334"/>
+        <source>translation</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="386"/>
+        <source>&amp;Ok</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="393"/>
+        <source>&amp;Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="400"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="407"/>
+        <source>Resets the preferences. Changes are applied when the buttons Ok or Apply are clicked.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.ui" line="410"/>
+        <source>&amp;Reset</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="preferences.cpp" line="130"/>
+        <source>Select kanji font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.cpp" line="137"/>
+        <source>Select okurigana font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.cpp" line="144"/>
+        <source>Select primary reading font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.cpp" line="151"/>
+        <source>Select primary translation font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.cpp" line="158"/>
+        <source>Select reading fFont</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="preferences.cpp" line="165"/>
+        <source>Select translation font</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vocablefactory.cpp" line="23"/>
+        <source>Could not open file &apos;%1&apos;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vocablefactory.cpp" line="47"/>
+        <source>readFromXML(): content is not &apos;vocables&apos; in line %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vocablefactory.cpp" line="56"/>
+        <source>readFromXML(): expected to read &lt;vocable&gt; or &lt;japovoc&gt; but read &lt;%1&gt; in line %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vocablefactory.cpp" line="87"/>
+        <source>parseVocable(): Expected to read &lt;vocable&gt; in line %1.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="vocablefactory.cpp" line="122"/>
+        <location filename="vocablefactory.cpp" line="143"/>
+        <location filename="vocablefactory.cpp" line="152"/>
+        <source>Read unexpected tag %1 in line %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
